8172c68649aa70806f450f9d1effbe15d35aca62
verydecent <wnjhwng@gmail.com>
2018-12-28 01:24:29 -0800

    sets up all crud operations, successfully tested

--- rest_api/server.js
+++ rest_api/server.js
@@ -1,19 +1,44 @@
 const express = require("express")
-const morgan = require("morgan")
+const logger = require("morgan")
 const bodyParser = require("body-parser")
-const errorHandler = require("errorhandler")
+// const errorHandler = require("errorhandler")
 
 // Instantiate express 
 const app = express()
 
+let store = {}
+store.accounts = []
+
 // Middleware
-app.use(morgan("dev"))
+app.use(logger("dev"))
+// app.use(errorHandler)
 app.use(bodyParser.json())
-app.use(errorHandler)
 
 // Testing server
 app.get("/", (req, res) => {
   res.send("Testing")
+})
+
+// Route handlers
+app.get('/accounts', (req, res) => {
+  res.status(200).send(store.accounts)
+})
+
+app.post('/accounts', (req, res) => {
+  let newAccount = req.body
+  let id = store.accounts.length
+  store.accounts.push(newAccount)
+  res.status(201).send({ id: id })
+})
+
+app.put('/accounts/:id', (req, res) => {
+  store.accounts[req.params.id] = req.body
+  res.status(200).send(store.accounts[req.params.id])
+})
+
+app.delete('/accounts/:id', (req, res) => {
+  store.accounts.splice(req.params.id, 1)
+  res.status(204).send()
 })
 
 // Instantiating port number in a variable
